2df613ba0c67ef0ccf84322d7d5b149412a9698a
Jiac-Xu <111858885+Jiac-Xu@users.noreply.github.com>
2024-01-26 08:51:12 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=13596926&pid=14bf2950f93c05efc461fbea94fcc973&tid=9b038f85763f4cf27f6c1e6ba47606d5",
+    "https://www.maimemo.com/share/page?uid=13596926&pid=095a70b425b3b1cbab248241825aab38&tid=cfe9be24f6bdfa360f99a90a0ab3ccbe",
 
 
   ]
